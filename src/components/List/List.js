5f0c14c8c634c4a8e00f751d3f13342a0340b6fc
khunzinshine <khunzinshine@ayainnovation.com>
2022-09-11 22:22:45 +0630

    add useMemo

---
+++ src/components/List/List.js
@@ -0,0 +1,25 @@
+import React, { useMemo } from 'react';
+import classes from './List.module.css';
+
+const List = ({items, title}) => {
+
+    //memorized items that no changes
+  const sortedList = useMemo(() => {
+    console.log('Items sorted');
+    return items.sort((a, b) => a - b);
+  }, [items]); 
+  console.log('LIST RUNNING');
+
+  return (
+    <div className={classes.list}>
+      <h2>{title}</h2>
+      <ul>
+        {sortedList.map((item) => (
+          <li key={item}>{item}</li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default React.memo(List);
